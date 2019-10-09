f1f9e34fb3a5acd0df423d6ecb529409aca9ee65
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-09 22:57:09 +0900

    create text.json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import text from './../text.json'
 const App = () => {
   return(
     <div className="container">
-      <h1 className="title">Engineering Students Presentation</h1>
+      <h1 className="title">{text.title}</h1>
       <p className="attention"></p>
       <div className="logo-wrapper">
-        <img src="./seedkun.png" alt="Seed-kun" />
+        <img src={text.image_file} alt={text.image_alt} />
       </div>
     </div>
   )
