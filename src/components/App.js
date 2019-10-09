d87bd4828a6aa6585390a7469db8e693ca2b6f36
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-09 22:48:14 +0900

    create top page

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const App = () => {
+  return(
+    <div className="container">
+      <h1 className="title">Engineering Students Presentation</h1>
+      <p className="attention"></p>
+      <div className="logo-wrapper">
+        <img src="./seedkun.png" alt="Seed-kun" />
+      </div>
+    </div>
+  )
+}
+export default App
