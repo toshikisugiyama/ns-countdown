5229ccb79c066e3ccc25c64233c593f41225ddec
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-10 17:02:25 +0900

    fixed count part (not yet)

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,17 +1,33 @@
 import React from 'react'
 const Count = () => {
-  let start = new Date(2019, 9, 10, 17, 0, 0)
-  showCountdown = () => {
-    let now = Date.now()
-    let elapsed = start.getTime() - now
-    msToTime(elapsed)
-  }
-  msToTime = duration => {
-    let seconds = parseInt((duration/1000)%60)
-    let minutes = parseInt((duration/(1000*60)%60)
-    minutes = (minutes<10)?"0"+minutes:minutes
-    seconds = (seconds<10)?"0"+seconds:seconds
-    return minutes+':'+seconds
+  const showCountdown = () => {
+    let now = new Date()
+    let target = new Date(2019, 9, 10, 17, 0, 0)
+    let elapsed = target - now.getTime()
+    let seconds = Math.floor(elapsed/1000)
+    let minuts = Math.floor(seconds/60)
+    let s
+    let m
+    switch (true) {
+      case (minuts<0):
+        s = (('0' + 0).slice(-2))
+        m = 0
+        break
+      case ((seconds%60<10) && (seconds>=60)):
+        s = (('0' + (seconds - Math.floor(seconds / 60)*60)).slice(-2))
+        m = minuts
+        break
+      case (seconds>=60):
+      case (seconds%60<10):
+        s = (seconds - Math.floor(seconds / 60)*60)
+        m = minuts
+        break
+      default:
+        s = seconds
+        m = minuts
+        break;
+    }
+    return m + ':' + s
   }
   return(
     <div className="count">
