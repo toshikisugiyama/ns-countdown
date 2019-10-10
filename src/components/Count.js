0ae6e2fb2390f0b11921d90bd0351aa62e061585
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-10 13:22:26 +0900

    count-part with error "Parsing error: Unexpected token" before "npm install --save-dev babel-eslint"

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,8 +1,21 @@
 import React from 'react'
 const Count = () => {
+  let start = new Date(2019, 9, 10, 17, 0, 0)
+  showCountdown = () => {
+    let now = Date.now()
+    let elapsed = start.getTime() - now
+    msToTime(elapsed)
+  }
+  msToTime = duration => {
+    let seconds = parseInt((duration/1000)%60)
+    let minutes = parseInt((duration/(1000*60)%60)
+    minutes = (minutes<10)?"0"+minutes:minutes
+    seconds = (seconds<10)?"0"+seconds:seconds
+    return minutes+':'+seconds
+  }
   return(
     <div className="count">
-      <p>5:00</p>
+      <p>{showCountdown()}</p>
     </div>
   )
 }
