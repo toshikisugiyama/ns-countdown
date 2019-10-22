e5374fdb2d184b6d6b8110e25a19d577c1d94aff
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-22 14:44:11 +0900

    fix modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,59 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import Count from './Count'
 import Modal from './Modal'
 import text from './../text.json'
 const App = () => {
-  const [time, setTime] = useState({
-    hour: 17,
-    minut: 0
-  })
+  const [hour, setHour] = useState(17)
+  const [minut, setMinut] = useState(0)
   const [modal, setModal] = useState(true)
-  const submitButton = () => {
+  let h, m
+  if(hour<10 && minut<10){
+    h = "0" + hour
+    m = "0" + minut
+  } else if(hour>=10 && minut<10) {
+    h = hour
+    m = "0" + minut
+  } else if(hour<10 && minut>=10) {
+    h = "0" + hour
+    m = minut
+  } else {
+    h = hour
+    m = minut
+  }
+  useEffect(() => {
+    if(modal===true){
+      document.getElementById('hour').value = h
+      document.getElementById('minut').value = m
+    }
+  })
+  const handleSubmitButton = () => {
     setModal(false)
+  }
+  const handlePlussHour = () => {
+    setHour(hour + 1)
+  }
+  const handleMinusHour = () => {
+    setHour(hour - 1)
+  }
+  const handlePlussMinut = () => {
+    setMinut(minut + 1)
+  }
+  const handleMinusMinut = () => {
+    setMinut(minut - 1)
   }
   return(
     <div className="container">
       <Modal
         modal={modal}
-        onChange={setTime}
-        submit={submitButton}
-        hour={time.hour}
-        minut={time.minut}
+        changePlussHour={handlePlussHour}
+        changeMinusHour={handleMinusHour}
+        changePlussMinut={handlePlussMinut}
+        changeMinusMinut={handleMinusMinut}
+        submit={handleSubmitButton}
       />
       <Count
-        hour={time.hour}
-        minut={time.minut}
+        hour={hour}
+        minut={minut}
       />
       <p className="attention">{text.attention}</p>
       <div className="logo-wrapper">
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,44 +1,35 @@
-import React,{useState} from 'react'
+import React from 'react'
 import text from '../text.json'
 const Modal = props => {
   const modal = props.modal
-  let hour
-  let minut
-  if(props.hour<10 && props.minut<10){
-    hour = "0" + props.hour
-    minut = "0" + props.minut
-  } else if(props.hour>=10 && props.minut<10) {
-    hour = props.hour
-    minut = "0" + props.minut
-  } else if(props.hour<10 && props.minut>=10) {
-    hour = "0" + props.hour
-    minut = props.minut
-  } else {
-    hour = props.hour
-    minut = props.minut
-  }
-  console.log(hour + ":" + minut)
-  console.log(document.getElementById('hour'))
-  console.log(document.getElementById('minut'))
-  // const h = document.getElementById('hour').value
-  // const m = document.getElementById('minut').value
+  const changePlussHour = props.changePlussHour
+  const changeMinusHour = props.changeMinusHour
+  const changePlussMinut = props.changePlussMinut
+  const changeMinusMinut = props.changeMinusMinut
+  const submit = props.submit
   if(modal===true){
     return(
       <div>
         <div className="modal-layer"></div>
         <div className="modal-contents">
-          <label>
-            <p>
-              {text.modal.title}
-            </p>
-            <div className="input">
-              <input id="hour" type="number" onChange={props.onChange({hour: 1})} />
-              <span>:</span>
-              <input id="minut" type="number" onChange={props.onChange({minut: 2})} />
+          <p>
+            {text.modal.title}
+          </p>
+          <div className="input">
+            <div className="time-container">
+              <button onClick={changePlussHour}>+</button>
+              <input id="hour" type="text" />
+              <button onClick={changeMinusHour}>-</button>
             </div>
-          </label>
+            <span>:</span>
+            <div className="time-container">
+              <button onClick={changePlussMinut}>+</button>
+              <input id="minut" type="text" />
+              <button onClick={changeMinusMinut}>-</button>
+            </div>
+          </div>
           <div className="button">
-            <button onClick={props.submit}>{text.modal.button}</button>
+            <button onClick={submit}>{text.modal.button}</button>
           </div>
         </div>
       </div>
